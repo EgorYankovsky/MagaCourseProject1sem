8940ea51071b49cacf565f667c61738e338b4dc2
Егор Янковский <yankovskij.2019@azure.nstu.ru>
2025-01-17 00:30:54 +0700

    generates above Y

--- Mesh/MeshGenerator.cpp
+++ Mesh/MeshGenerator.cpp
@@ -63,7 +63,7 @@ void MeshGenerator::GenerateListOfPoints(Mesh& mesh) {
                 figure[k][j][i] = mesh.immutablePoints_[k * sxy + j * lx + i];
 
 
-
+    // Generation above X-axis.
     for (size_t k(0); k < mesh.LinesAmountZ; ++k) {
         for (size_t j(0); j < mesh.LinesAmountY; ++j) {
             std::vector<Point> lineToBuild{};
@@ -108,7 +108,83 @@ void MeshGenerator::GenerateListOfPoints(Mesh& mesh) {
             figure[k][j] = lineToBuild;
         }
     }
-    //mesh.linesAmountX_ = figure[0][0].size();
+    mesh.linesAmountX_ = figure[0][0].size();
+
+    // Generation above Y-axis.
+    for (size_t k(0); k < mesh.LinesAmountZ; ++k) {
+        std::vector<std::vector<Point>> squareToBuild{};
+        squareToBuild = figure[k];
+        size_t shift = mesh.LinesAmountY - 1;
+        for (const auto info : mesh.delimitersY_) {
+            auto rightBorderIter = squareToBuild.end() - shift;
+            auto amountOfDelimiters = info.first;
+            auto coefficientOfDelimiter = info.second;
+
+            std::vector<Point> v0(mesh.LinesAmountX);
+            std::vector<Point> v1(mesh.LinesAmountX);
+            std::copy((*(rightBorderIter - 1)).begin(), (*(rightBorderIter - 1)).end(), v0.begin());
+            std::copy((*(rightBorderIter)).begin(), (*(rightBorderIter)).end(), v1.begin());
+
+            std::vector<std::vector<Point>> subSquareToBuild(amountOfDelimiters - 1);
+            for (auto& line : subSquareToBuild) line.resize(mesh.LinesAmountX);
+
+            double denum = 0.0;
+            for (size_t ii(0); ii < amountOfDelimiters; ++ii)
+                denum += pow(coefficientOfDelimiter, ii);
+
+
+            for (size_t i(0); i < mesh.LinesAmountX; ++i) {
+                double x0 = v0[i].x;
+                double x1 = v1[i].x;
+
+                double y0 = v0[i].y;
+                double y1 = v1[i].y;
+
+                double z0 = v0[i].z;
+                double z1 = v1[i].z;
+
+                double deltX = x1 - x0;
+                double deltY = y1 - y0;
+                double deltZ = z1 - z0;
+
+                double xh = deltX / denum;
+                double yh = deltY / denum;
+                double zh = deltZ / denum;
+
+                double multiplier = 0.0;
+                for (size_t ii(0); ii < amountOfDelimiters - 1; ++ii) {
+                    multiplier += pow(coefficientOfDelimiter, ii);
+                    auto pointToInsert = Point(x0 + xh * multiplier,
+                                               y0 + yh * multiplier,
+                                               z0 + zh * multiplier);
+
+                    subSquareToBuild[ii][i] = pointToInsert;
+                }
+            }
+
+            for (auto line : subSquareToBuild)
+                squareToBuild.insert(squareToBuild.end() - shift, line);
+            shift--;
+        }
+        figure[k] = squareToBuild;
+    }
+    mesh.linesAmountY_ = figure[0].size();
+
+    // Generate above Z-axis.
+    size_t shift = mesh.LinesAmountZ - 1;
+    for (const auto info : mesh.delimitersZ_) {
+
+    }
+
+    // Convert to line-format.
+    mesh.linesAmountZ_ = figure.size();
+    mesh.points_.resize(mesh.linesAmountX_ * mesh.linesAmountY_ * mesh.linesAmountZ_);
+    sxy = mesh.LinesAmountX * mesh.LinesAmountY;
+    lx = mesh.LinesAmountX;
+    for (size_t k(0); k < mesh.LinesAmountZ; ++k)
+        for (size_t j(0); j < mesh.LinesAmountY; ++j)
+            for (size_t i(0); i < mesh.LinesAmountX; ++i)
+                mesh.points_[k * sxy + j * lx + i] = figure[k][j][i];
 }
 
 void MeshGenerator::GenerateListOfAreas(Mesh& mesh) {
